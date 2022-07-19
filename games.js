2e1c675941cbc6a9b86dd507661818a05cf73462
brobin714 <brobin714@gmail.com>
2022-07-19 06:29:38 -0400

    Correct games code, index, and css paths

--- games.js
+++ games.js
@@ -1,7 +1,3 @@
-const express = require("express");
-const https = require('https');
-
-const app = express();
 
 let gamePattern = [];
 const buttonColors = ["red", "blue", "yellow", "green"];
@@ -10,11 +6,6 @@ let userClickedPattern = [];
 let level = 0;
 
 
-app.use(express.static("public")); // static ensures any files made work from the specified dir
-app.use(express.json()); // express took over bodyParser
-app.use(express.urlencoded({
-  extended: true
-})); // extended true uses any urlencoded type not just string
 
 //starts game with restart button click
 $(".start").on("click", function() {
@@ -99,42 +90,42 @@ function evolvePokemon(pokemon) {
   switch (pokemon) {
     case 'charmander':
       $("#red").switchClass("charmander", "charmeleon", 1000);
-      $("#red").css("background-image", "url(public/images/charmeleon.png)");
+      $("#red").css("background-image", "url(./images/charmeleon.png)");
       buttonPokemon[0] = "charmeleon";
       break;
     case 'charmeleon':
       $("#red").switchClass("charmeleon", "charizard", 1000);
-      $("#red").css("background-image", "url(public/images/charizard.png)");
+      $("#red").css("background-image", "url(./images/charizard.png)");
       buttonPokemon[0] = "charizard";
       break;
     case 'squirtle':
       $("#blue").switchClass("squirtle", "wartortle", 1000);
-      $("#blue").css("background-image", "url(public/images/wartortle.png)");
+      $("#blue").css("background-image", "url(./images/wartortle.png)");
       buttonPokemon[1] = "wartortle";
       break;
     case 'wartortle':
       $("#blue").switchClass("wartotle", "blastoise", 1000);
-      $("#blue").css("background-image", "url(public/images/blastoise.png)");
+      $("#blue").css("background-image", "url(./images/blastoise.png)");
       buttonPokemon[1] = "blastoise";
       break;
     case 'pichu':
       $("#yellow").switchClass("pichu", "pikachu", 1000);
-      $("#yellow").css("background-image", "url(public/images/pikachu.png)");
+      $("#yellow").css("background-image", "url(./images/pikachu.png)");
       buttonPokemon[2] = "pikachu";
       break;
     case 'pikachu':
       $("#yellow").switchClass("pikachu", "raichu", 1000);
-      $("#yellow").css("background-image", "url(public/images/raichu.png)");
+      $("#yellow").css("background-image", "url(./images/raichu.png)");
       buttonPokemon[2] = "raichu";
       break;
     case 'bulbasaur':
       $("#green").switchClass("bulbasaur", "ivysaur", 1000);
-      $("#green").css("background-image", "url(public/images/ivysaur.png)");
+      $("#green").css("background-image", "url(./images/ivysaur.png)");
       buttonPokemon[3] = "ivysaur";
       break;
     case 'ivysaur':
       $("#green").switchClass("ivysaur", "venusaur", 1000);
-      $("#green").css("background-image", "url(public/images/venusaur.png)");
+      $("#green").css("background-image", "url(./images/venusaur.png)");
       buttonPokemon[3] = "venusaur";
       break;
     default:
@@ -194,10 +185,10 @@ function resetGame() {
   buttonPokemon = ["charmander", "squirtle", "pikachu", "bulbasaur"];
   userClickedPattern = [];
   level = 0;
-  $("#red").css("background-image", "url(public/images/charmander.png)");
-  $("#blue").css("background-image", "url(public/images/squirtle.png)");
-  $("#yellow").css("background-image", "url(public/images/pichu.png)");
-  $("#green").css("background-image", "url(public/images/bulbasaur.png)");
+  $("#red").css("background-image", "url(./images/charmander.png)");
+  $("#blue").css("background-image", "url(./images/squirtle.png)");
+  $("#yellow").css("background-image", "url(./images/pichu.png)");
+  $("#green").css("background-image", "url(./images/bulbasaur.png)");
   $("#red").removeClass("charizard charmeleon charmander").addClass("charmander");
   $("#blue").removeClass("blastoise wartortle squirtle").addClass("squirtle");
   $("#yellow").removeClass("raichu pikachu pichu").addClass("pichu");
